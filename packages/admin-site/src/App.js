d4f895220c3d01f5f748bcfd6f3895d7dd0ee6c7
changqing <qing.chang@eeoa.com>
2022-10-29 14:08:41 +0800

    1

---
+++ packages/admin-site/src/App.js
@@ -0,0 +1,13 @@
+import {WaterForm} from '@mono-repo-react/common';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>管理员站点</h1>
+      <WaterForm />
+    </div>
+  );
+}
+
+export default App;
